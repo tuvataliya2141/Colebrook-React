a13783c198230c9aa6be65b80f0ec9e08a9ed0ed
brijesh9795 <brijeshvataliya9795@gmail.com>
2022-12-21 18:57:31 +0530

    home page design

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -16,6 +16,7 @@ function Footer() {
                     <ul className="contact-infor">
                     <li><img src="assets/imgs/theme/icons/icon-location.svg" alt="/"/><strong>Address: </strong> <span>5171 W Campbell Ave undefined Kent, Utah 53127 United States</span></li>
                     <li><img src="assets/imgs/theme/icons/icon-contact.svg" alt="/"/><strong>Call Us:</strong><span>(+91) - 540-025-124553</span></li>
+                    <li><img src="assets/imgs/theme/icons/icon-contact.svg" alt="/"/><strong>Call Us:</strong><span>(+91) - 540-025-124553</span></li>
                     <li><img src="assets/imgs/theme/icons/icon-email-2.svg" alt="/"/><strong>Email:</strong><span>sale@Nest.com</span></li>
                     <li><img src="assets/imgs/theme/icons/icon-clock.svg" alt="/"/><strong>Hours:</strong><span>10:00 - 18:00, Mon - Sat</span></li>
                     </ul>
