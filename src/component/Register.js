b89da5ab6b41c055acb36714a985c115fd753823
brijesh9795 <brijeshvataliya9795@gmail.com>
2022-12-23 21:14:10 +0530

    css

--- src/component/Register.js
+++ src/component/Register.js
@@ -56,7 +56,7 @@ function Register() {
             <div className="container">
             <div className="breadcrumb">
                 <a href="index.html" rel="nofollow"><i className="fi-rs-home mr-5" />Home</a>
-                <span /> Pages <span /> My Account
+                <span /> Create an Account <span /> My Account
             </div>
             </div>
         </div>
@@ -74,49 +74,38 @@ function Register() {
                         </div>
                         <form method="post">
                             <div className="form-group">
-                            <input type="text" required name="Name" placeholder="Name" value={name} onChange={(e) => {SetName(e.target.value)}}/>
+                                <input type="text" required name="Name" placeholder="Name" value={name} onChange={(e) => {SetName(e.target.value)}}/>
                             </div>
                             <div className="form-group">
-                            <input type="text" required name="email" placeholder="Email" value={email_or_phone} onChange={(e) => {SetEmail_or_phone(e.target.value)}} />
+                                <input type="text" required name="email" placeholder="Email" value={email_or_phone} onChange={(e) => {SetEmail_or_phone(e.target.value)}} />
                             </div>
                             <div className="form-group">
-                            <input required type="password" name="password" placeholder="Password" value={password} onChange={(e) => {SetPassword(e.target.value)}} />
+                                <input required type="password" name="password" placeholder="Password" value={password} onChange={(e) => {SetPassword(e.target.value)}} />
                             </div>
                             <div className="form-group">    
-                            <input required type="password" name="password" placeholder="Confirm password"  value={password} onChange={(e) => {SetPassword(e.target.value)}}  />
-                            </div>
-                            <div className="login_footer form-group">
-                            {/* <div className="chek-form">
-                                <input type="text" required name="email" placeholder="Security code *" />
-                            </div>
-                            <span className="security-code">
-                                <b className="text-new">8</b>
-                                <b className="text-hot">6</b>
-                                <b className="text-sale">7</b>
-                                <b className="text-best">5</b>
-                            </span> */}
+                                <input required type="password" name="password" placeholder="Confirm password"  value={password} onChange={(e) => {SetPassword(e.target.value)}}  />
                             </div>
                             <div className="payment_option mb-50">
-                            <div className="custome-radio">
-                                <input className="form-check-input" required type="radio" name="payment_option" id="exampleRadios3" defaultChecked />
-                                <label className="form-check-label" htmlFor="exampleRadios3" data-bs-toggle="collapse" data-target="#bankTranfer" aria-controls="bankTranfer">I am a customer</label>
-                            </div>
-                            <div className="custome-radio">
-                                <input className="form-check-input" required type="radio" name="payment_option" id="exampleRadios4" defaultChecked />
-                                <label className="form-check-label" htmlFor="exampleRadios4" data-bs-toggle="collapse" data-target="#checkPayment" aria-controls="checkPayment">I am a vendor</label>
-                            </div>
-                            </div>
-                            <div className="login_footer form-group mb-50">
-                            <div className="chek-form">
-                                <div className="custome-checkbox">
-                                <input className="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox12" defaultValue />
-                                <label className="form-check-label" htmlFor="exampleCheckbox12"><span>I agree to terms &amp; Policy.</span></label>
+                                <div className="custome-radio">
+                                    <input className="form-check-input" required type="radio" name="payment_option" id="exampleRadios3" defaultChecked />
+                                    <label className="form-check-label" htmlFor="exampleRadios3" data-bs-toggle="collapse" data-target="#bankTranfer" aria-controls="bankTranfer">I am a customer</label>
+                                </div>
+                                <div className="custome-radio">
+                                    <input className="form-check-input" required type="radio" name="payment_option" id="exampleRadios4" defaultChecked />
+                                    <label className="form-check-label" htmlFor="exampleRadios4" data-bs-toggle="collapse" data-target="#checkPayment" aria-controls="checkPayment">I am a vendor</label>
                                 </div>
                             </div>
-                            <a href="page-privacy-policy.html"><i className="fi-rs-book-alt mr-5 text-muted" />Lean more</a>
+                            <div className="login_footer form-group mb-50">
+                                <div className="chek-form">
+                                    <div className="custome-checkbox">
+                                    <input className="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox12" defaultValue />
+                                    <label className="form-check-label" htmlFor="exampleCheckbox12"><span>I agree to terms &amp; Policy.</span></label>
+                                    </div>
+                                </div>
+                                <a href="page-privacy-policy.html"><i className="fi-rs-book-alt mr-5 text-muted" />Lean more</a>
                             </div>
                             <div className="form-group mb-30">
-                            <button type="submit" className="btn btn-fill-out btn-block hover-up font-weight-bold" onClick={SubmitData} name="login">Submit &amp; Register</button>
+                                <button type="submit" className="btn btn-fill-out btn-block hover-up font-weight-bold" onClick={SubmitData} name="login">Submit &amp; Register</button>
                             </div>
                             <p className="font-xs text-muted"><strong>Note:</strong>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our privacy policy</p>
                         </form>
