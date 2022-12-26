b4bdde005f058163b9e8aa366bf2f4658fc3dd9f
brijesh9795 <brijeshvataliya9795@gmail.com>
2022-12-26 14:09:11 +0530

    regiser page

--- src/component/Register.js
+++ src/component/Register.js
@@ -56,79 +56,76 @@ function Register() {
             <div className="container">
             <div className="breadcrumb">
                 <a href="index.html" rel="nofollow"><i className="fi-rs-home mr-5" />Home</a>
-                <span /> Create an Account <span /> My Account
+                <span /> Create an Account <span />
             </div>
             </div>
         </div>
         <div className="page-content pt-150 pb-150">
             <div className="container">
             <div className="row">
+                <div className="heading_s1">
+                    <h1 className="mb-5">Create an Account</h1>
+                    <p className="mb-30">Already have an account? <Link to="/Login">Login</Link></p>
+                </div>
                 <div className="col-xl-8 col-lg-10 col-md-12 m-auto">
-                <div className="row">
-                    <div className="col-lg-6 col-md-8">
-                    <div className="login_wrap widget-taber-content background-white">
-                        <div className="padding_eight_all bg-white">
-                        <div className="heading_s1">
-                            <h1 className="mb-5">Create an Account</h1>
-                            <p className="mb-30">Already have an account? <Link to="/Login">Login</Link></p>
-                        </div>
-                        <form method="post">
-                            <div className="form-group">
-                                <input type="text" required name="Name" placeholder="Name" value={name} onChange={(e) => {SetName(e.target.value)}}/>
+                    <div className="row">
+                        <div className="col-lg-6 col-md-8">
+                            <div className="login_wrap widget-taber-content background-white">
+                                <div className="padding_eight_all bg-white">
+                                {/* <form method="post"> */}
+                                    <div className="form-group">
+                                        <input type="text" required name="Name" placeholder="Name" value={name} onChange={(e) => {SetName(e.target.value)}}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input required type="password" name="password" placeholder="Password" value={password} onChange={(e) => {SetPassword(e.target.value)}} />
+                                    </div>
+                                </div>
                             </div>
+                        </div>
+                        <div className="col-lg-6 col-md-8">
                             <div className="form-group">
                                 <input type="text" required name="email" placeholder="Email" value={email_or_phone} onChange={(e) => {SetEmail_or_phone(e.target.value)}} />
-                            </div>
-                            <div className="form-group">
-                                <input required type="password" name="password" placeholder="Password" value={password} onChange={(e) => {SetPassword(e.target.value)}} />
                             </div>
                             <div className="form-group">    
                                 <input required type="password" name="password" placeholder="Confirm password"  value={password} onChange={(e) => {SetPassword(e.target.value)}}  />
                             </div>
-                            <div className="payment_option mb-50">
-                                <div className="custome-radio">
-                                    <input className="form-check-input" required type="radio" name="payment_option" id="exampleRadios3" defaultChecked />
-                                    <label className="form-check-label" htmlFor="exampleRadios3" data-bs-toggle="collapse" data-target="#bankTranfer" aria-controls="bankTranfer">I am a customer</label>
-                                </div>
-                                <div className="custome-radio">
-                                    <input className="form-check-input" required type="radio" name="payment_option" id="exampleRadios4" defaultChecked />
-                                    <label className="form-check-label" htmlFor="exampleRadios4" data-bs-toggle="collapse" data-target="#checkPayment" aria-controls="checkPayment">I am a vendor</label>
-                                </div>
-                            </div>
-                            <div className="login_footer form-group mb-50">
-                                <div className="chek-form">
-                                    <div className="custome-checkbox">
-                                    <input className="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox12" defaultValue />
-                                    <label className="form-check-label" htmlFor="exampleCheckbox12"><span>I agree to terms &amp; Policy.</span></label>
-                                    </div>
-                                </div>
-                                <a href="page-privacy-policy.html"><i className="fi-rs-book-alt mr-5 text-muted" />Lean more</a>
-                            </div>
-                            <div className="form-group mb-30">
-                                <button type="submit" className="btn btn-fill-out btn-block hover-up font-weight-bold" onClick={SubmitData} name="login">Submit &amp; Register</button>
-                            </div>
-                            <p className="font-xs text-muted"><strong>Note:</strong>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our privacy policy</p>
-                        </form>
                         </div>
                     </div>
+                    <div className="row">
+                        <div className="col-lg-6 col-md-8">
+                            <div className="login_wrap widget-taber-content background-white">
+                                <div className="payment_option mb-50">
+                                    <div className="custome-radio">
+                                        <input className="form-check-input" required type="radio" name="payment_option" id="exampleRadios3" defaultChecked />
+                                        <label className="form-check-label" htmlFor="exampleRadios3" data-bs-toggle="collapse" data-target="#bankTranfer" aria-controls="bankTranfer">I am a customer</label>
+                                    </div>
+                                    <div className="custome-radio">
+                                        <input className="form-check-input" required type="radio" name="payment_option" id="exampleRadios4" defaultChecked />
+                                        <label className="form-check-label" htmlFor="exampleRadios4" data-bs-toggle="collapse" data-target="#checkPayment" aria-controls="checkPayment">I am a vendor</label>
+                                    </div>
+                                </div>
+                                <div className="login_footer form-group mb-50">
+                                    <div className="chek-form">
+                                        <div className="custome-checkbox">
+                                        <input className="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox12" defaultValue />
+                                        <label className="form-check-label" htmlFor="exampleCheckbox12"><span>I agree to terms &amp; Policy.</span></label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="font-xs text-muted"><strong>Note:</strong>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our privacy policy</p>
+                            {/* </form> */}
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-8 mt-20">
+                            <div className="form-group mb-30">
+                                <span>
+                                    <a href='#' className="btn btn-heading btn-block fb-btn" name="fb" style={{backgroundColor: "#1877f2"}}><img src="assets/imgs/theme/icons/logo-facebook.svg" alt="/"/></a>
+                                    <a href='#' className="btn btn-heading btn-block google-btn" name="google" style={{backgroundColor: "#fff"}}><img src="assets/imgs/theme/icons/logo-google.svg" alt="/"/></a>
+                                </span>
+                                <button type="submit" style={{borderRadius: "30px",float: "right"}} className="btn btn-fill-out btn-block hover-up font-weight-bold" onClick={SubmitData} name="login">Submit &amp; Register</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-lg-6 pr-30 d-none d-lg-block">
-                    <div className="card-login mt-115">
-                        <a href="#" className="social-login facebook-login">
-                        <img src="assets/imgs/theme/icons/logo-facebook.svg" alt="/"/>
-                        <span>Continue with Facebook</span>
-                        </a>
-                        <a href="#" className="social-login google-login">
-                        <img src="assets/imgs/theme/icons/logo-google.svg" alt="/"/>
-                        <span>Continue with Google</span>
-                        </a>
-                        <a href="#" className="social-login apple-login">
-                        <img src="assets/imgs/theme/icons/logo-apple.svg" alt="/"/>
-                        <span>Continue with Apple</span>
-                        </a>
-                    </div>
-                    </div>
-                </div>
                 </div>
             </div>
             </div>
